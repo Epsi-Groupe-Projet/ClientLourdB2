e5d1065ee496f2b594f036477c07f73bc4c4a4f2
Yassine Faris <yassine.faris@epsi.fr>
2014-12-04 10:53:53 +0100

    Ajout del fonction qui fout rien

--- RH/Function.py
+++ RH/Function.py
@@ -1,9 +1,13 @@
 from ConnexionMysql import *
 import Parametre
 
-def Connection(resultat):
+def Connection(resultats):
+	resultat = resultats['listResultEntry']
 	connexion = ConnexionServeur(Parametre.host,resultat[0].get(),resultat[1].get())
 	if connexion.GetConnexion != None:
 		return connexion
 	else:
 		return False
+
+def FonctionQuiFoutRien(resultat):
+	return None
